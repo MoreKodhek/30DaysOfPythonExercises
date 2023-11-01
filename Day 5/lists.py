0f71df46cbf0474375fad70434657cff04413a82
More Argwings Kodhek <morekodhek@gmail.com>
2023-11-01 19:04:49 +0300

    Fix

---
+++ Day 5/lists.py
@@ -0,0 +1,17 @@
+empty_list = []
+my_list = [1, 2, 3, 4, 5]
+print(len(my_list))
+first_num = my_list[0]
+mid_num = my_list[2]
+last_num = my_list[4]
+print(first_num, mid_num, last_num)
+
+mixed_data_types = ["More", 33, "6feet", "at home"]
+it_companies = ["Facebook", "Google", "Microsoft",
+                "Apple", "IBM", "Oracle", "Amazon"]
+print(it_companies)
+print(len(it_companies))
+first_comp = it_companies[0]
+mid_comp = it_companies[3]
+last_comp = it_companies[6]
+print(first_comp, mid_comp, last_comp)
